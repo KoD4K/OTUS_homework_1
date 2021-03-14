c33a000595bbdf2c7774581d97a32dacda0599f8
Evgenii Plenkin <free.for.message@gmail.com>
2021-03-14 20:30:36 +0300

    First commit

---
+++ Home_1/Home_1/Home_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Home_1App.swift
+//  Home_1
+//
+//  Created by Evgenii Plenkin on 14.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Home_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
